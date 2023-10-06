6ee9ec51909cdcb48025f8e4865c11035c58746c
Piyush696 <piyushdutta696@gmail.com>
2023-10-06 12:00:21 +0530

    first commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -22,43 +22,68 @@ const Sidebar = () => {
             x: 0,
 
             transition: {
-                delay: 0.1, duration: 0.5, type: 'spring'
+                delay: 0.1,
+                duration: 0.5,
+                type: 'spring'
             }
         }
     }
 
 
-    return (
-        <motion.div className="sidebar"
-            variants={sidebar_variant}
-            initial='hidden'
-            animate="visible"
+    return ( <
+        motion.div className = "sidebar"
+        variants = { sidebar_variant }
+        initial = 'hidden'
+        animate = "visible"
 
         >
-            <img src={mightycoder} alt="avatar" className="sidebar__avatar" />
-            <div className="sidebar__name">Sumit <span>Dey</span> </div>
-            <div className="sidebar__item sidebar__title">Web Developer</div>
-            <a href={resume} download="resume.pdf">
-                <div className="sidebar__item sidebar__resume">
-                    <img src={tie} alt="resume" className="sidebar__icon" />Download Resume
-                </div>
-            </a>
-            <figure className="sidebar__social-icons my-2">
-                <a href="!#"><img src={facebook} alt="facebook" className="sidebar__icon mr-3" /></a>
-                <a href="!#"><img src={instagram} alt="instagram" className="sidebar__icon" /></a>
-            </figure>
-            <div className="sidebar__contact">
-                <div className="sidebar__item sidebar__github">
-                    <a href="!#"><img src={github} alt="github" className="sidebar__icon mr-3" />github</a>
-                </div>
-                <div className="sidebar__location">
-                    <img src={pin} alt="location" className="sidebar__icon mr-3" />
-                    Kolkata, India</div>
-                <div className="sidebar__item">code.sumax@gmail.com</div>
-                <div className="sidebar__item">7835323423/9876543510</div>
-            </div>
-            <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>email me</div>
-        </motion.div>
+        <
+        img src = { mightycoder }
+        alt = "avatar"
+        className = "sidebar__avatar" / >
+        <
+        div className = "sidebar__name" > Piyush < span > Dutta < /span> </div >
+        <
+        div className = "sidebar__item sidebar__title" > Web Developer < /div> <
+        a href = { resume }
+        download = "resume.pdf" >
+        <
+        div className = "sidebar__item sidebar__resume" >
+        <
+        img src = { tie }
+        alt = "resume"
+        className = "sidebar__icon" / > Download Resume <
+        /div> < /
+        a > <
+        figure className = "sidebar__social-icons my-2" >
+        <
+        a href = "!#" > < img src = { facebook }
+        alt = "facebook"
+        className = "sidebar__icon mr-3" / > < /a> <
+        a href = "!#" > < img src = { instagram }
+        alt = "instagram"
+        className = "sidebar__icon" / > < /a> < /
+        figure > <
+        div className = "sidebar__contact" >
+        <
+        div className = "sidebar__item sidebar__github" >
+        <
+        a href = "!#" > < img src = { github }
+        alt = "github"
+        className = "sidebar__icon mr-3" / > github < /a> < /
+        div > <
+        div className = "sidebar__location" >
+        <
+        img src = { pin }
+        alt = "location"
+        className = "sidebar__icon mr-3" / >
+        Kolkata, India < /div> <
+        div className = "sidebar__item" > iampiyushdutta @gmail.com < /div> <
+        div className = "sidebar__item" > 8340172667 < /div> < /
+        div > <
+        div className = "sidebar__item sidebar__email"
+        onClick = { handleEmailMe } > email me < /div> < /
+        motion.div >
     )
 }
 
